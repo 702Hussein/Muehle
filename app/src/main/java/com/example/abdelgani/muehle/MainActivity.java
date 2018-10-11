13a03340f6be4c707157bfbf19cfa79c3301c03a
Hussein Abdelgani <abdelgani@itq.de>
2018-10-11 15:55:09 +0200

    Created the Login layout not finish yet

--- app/src/main/java/com/example/abdelgani/muehle/MainActivity.java
+++ app/src/main/java/com/example/abdelgani/muehle/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.abdelgani.muehle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText password;
+    private Button Login;
+
+    Button mButton = (Button) findViewById(R.id.btnLoginID);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        Name = (EditText)findViewById( R.id.NameID );
+        password = (EditText)findViewById( R.id.passwordID );
+        Login = (Button)findViewById( R.id.btnLoginID );
+
     }
+//https://www.youtube.com/watch?v=lF5m4o_CuNg
+
+    private void validate (String userName, String userPassword)
+    {
+
+    }
+
+    
+
 }

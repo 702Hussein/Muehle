ab73c51604e197b41a9b864125d6da9279d976fa
Hussein Abdelgani <abdelgani@itq.de>
2018-10-17 18:56:12 +0200

    created: - Second Activity (activity name should be changed) - SinglePlayer Activity - MultiyPlaer Activity
    Buttons:
    - back
    - SinglePlayer
    - MultiyPlaer

--- app/src/main/java/com/example/abdelgani/muehle/MainActivity.java
+++ app/src/main/java/com/example/abdelgani/muehle/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 Intent OpenSecondActivity = new Intent( MainActivity.this, SecondActivity.class);
+                Toast.makeText( getApplicationContext(), "Login Successfuly", Toast.LENGTH_SHORT ).show();
                 startActivity( OpenSecondActivity );
             }
         } );

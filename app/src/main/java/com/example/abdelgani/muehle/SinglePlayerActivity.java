d6dcf32e60ac488f63f8e638e8326811d7faa2bf
Hussein Abdelgani <abdelgani@itq.de>
2018-12-05 11:11:30 +0100

    Betätigungsmeldung beim Verlassen des Spielfeldes

--- app/src/main/java/com/example/abdelgani/muehle/SinglePlayerActivity.java
+++ app/src/main/java/com/example/abdelgani/muehle/SinglePlayerActivity.java
@@ -1,33 +1,61 @@
 package com.example.abdelgani.muehle;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import java.io.Console;
 
 public class SinglePlayerActivity extends AppCompatActivity
 {
-
     Database seconddatabase ;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_single_player );
-
-        String ich;
-        TextView text;
-
-         text = (TextView)findViewById( R.id.PlayerName );
-
-         //ich = getString(  );
-
-       // ich = Name.getText().toString();
-
-        text.setText("name ist : ");
-
     }
+
+
+    // Zurück zur SecondActivity fall die Rückwärts taste gedrückt wird.
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if (keyCode == KeyEvent.KEYCODE_BACK)
+        {
+            AlertDialog.Builder dlgBuilder = new AlertDialog.Builder(SinglePlayerActivity.this);
+
+            dlgBuilder.setMessage("Sind sie sicher dass Sie das Spiel verlassen wollen?");
+
+            dlgBuilder.setCancelable(true);
+            dlgBuilder.setPositiveButton("Ja", new DialogInterface.OnClickListener()
+            {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                Toast.makeText(SinglePlayerActivity.this, "Das Spiel wurde beendet", Toast.LENGTH_SHORT).show();
+                Intent secondAct = new Intent( SinglePlayerActivity.this, SecondActivity.class );
+                startActivity( secondAct );
+            }
+        });
+            dlgBuilder.setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+            AlertDialog alert = dlgBuilder.create();
+            alert.getWindow().setBackgroundDrawable(new ColorDrawable(Color.GREEN));
+            alert.show();
+        }
+        return true;
+    }
+
 }

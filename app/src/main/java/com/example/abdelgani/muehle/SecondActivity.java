31569180f1d336e5a957f1092da13e798143733b
Hussein Abdelgani <abdelgani@itq.de>
2018-12-14 21:53:59 +0100

    PopUp Felder für die Anmeldung 2. Spielers ohne neue activity zu öffnen

--- app/src/main/java/com/example/abdelgani/muehle/SecondActivity.java
+++ app/src/main/java/com/example/abdelgani/muehle/SecondActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity
@@ -60,8 +61,35 @@ public class SecondActivity extends AppCompatActivity
     {
                 //Intent OpenMultiPlayerActivity = new Intent( SecondActivity.this, MultiPlayerActivity.class );
                 //startActivity( OpenMultiPlayerActivity );
-        Intent openPopup = new Intent( SecondActivity.this, Popup.class );
-        startActivity( openPopup );
+        //Intent openPopup = new Intent( SecondActivity.this, Popup.class );
+        //startActivity( openPopup );
+        android.app.AlertDialog.Builder mBuilder = new android.app.AlertDialog.Builder( SecondActivity.this );
+        View popUp_View = getLayoutInflater().inflate( R.layout.second_player_popup, null );
+        final EditText userName = (EditText) popUp_View.findViewById( R.id.userName );
+        final EditText password = (EditText) popUp_View.findViewById( R.id.password );
+        Button logIn = (Button) popUp_View.findViewById( R.id.Login_popup);
+
+        logIn.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if(!userName.getText().toString().isEmpty() && !password.getText().toString().isEmpty())
+                {
+                    Toast.makeText( SecondActivity.this, "Login Succefully", Toast.LENGTH_SHORT ).show();
+                }
+                else
+                {
+                    Toast.makeText( SecondActivity.this, "Files are empty", Toast.LENGTH_SHORT ).show();
+                }
+            }
+        } );
+
+        mBuilder.setView( popUp_View );
+
+        android.app.AlertDialog dialog = mBuilder.create();
+        dialog.show();
+
     }
 
     public void onBackPressed()
